9f83918b9dcf4a8ced8bddbe697010a8e6ee78a3
9731077 <9731077@ceit.local>
2019-04-26 11:46:03 +0430

    field added

--- src/Chessman.java
+++ src/Chessman.java
@@ -1,2 +1,39 @@
 public class Chessman {
+    private String name;
+    private String color;
+    private String alphabetCoordinate;
+    private int numCoordinate;
+
+    public int getNumCoordinate() {
+        return numCoordinate;
+    }
+
+    public void setNumCoordinate(int numCoordinate) {
+        this.numCoordinate = numCoordinate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAlphabetCoordinate() {
+        return alphabetCoordinate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setAlphabetCoordinate(String alphabetCoordinate) {
+        this.alphabetCoordinate = alphabetCoordinate;
+    }
+
 }
--- src/Field.java
+++ src/Field.java
@@ -1,2 +1,4 @@
 public class Field {
+    private int[][] field = new int[8][8];
+
 }
