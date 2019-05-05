b8ab35a8b9f76d40eec32482731f1d651d694a6f
9731077 <9731077@ceit.local>
2019-05-05 10:34:00 +0430

    Bishop checker added

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,13 +1,66 @@
+import java.util.ArrayList;
+import java.lang.Math;
+
 public class Bishop extends Chessman {
     public Bishop(String color, int alphabetCoordinate, int numCoordinate) {
         super(color, alphabetCoordinate, numCoordinate);
+    }
+
+    public boolean check(int alpCoordinate, int numCordinate, ArrayList<Chessman> menArr) {
+        int n, m;
+        n = alpCoordinate - getAlphabetCoordinate();
+        m = numCordinate - getNumCoordinate();
+        for (Chessman chessman : menArr) {
+            if (chessman.getAlphabetCoordinate() == getAlphabetCoordinate() && chessman.getNumCoordinate() == getNumCoordinate()) {
+                if (chessman.getColor() == getColor())
+                    return false;
+            }
+        }
+        if (n > 0 && m > 0) {
+            for (int i = 0; i != Math.sqrt(n); i++) {
+                for (Chessman chessman : menArr) {
+                    if (chessman.getNumCoordinate() == getNumCoordinate() + i && chessman.getAlphabetCoordinate() == getAlphabetCoordinate() + i)
+                        return false;
+                }
+            }
+
+        }
+        if (n > 0 && m < 0) {
+            for (int i = 0; i != Math.sqrt(n); i++) {
+                for (Chessman chessman : menArr) {
+                    if (chessman.getNumCoordinate() == getNumCoordinate() - i && chessman.getAlphabetCoordinate() == getAlphabetCoordinate() + i)
+                        return false;
+                }
+            }
+
+        }
+        if (n < 0 && m > 0) {
+            for (int i = 0; i != Math.sqrt(n); i++) {
+                for (Chessman chessman : menArr) {
+                    if (chessman.getNumCoordinate() == getNumCoordinate() + i && chessman.getAlphabetCoordinate() == getAlphabetCoordinate() - i)
+                        return false;
+                }
+            }
+
+        }
+        if (n < 0 && m < 0) {
+            for (int i = 0; i != Math.sqrt(n); i++) {
+                for (Chessman chessman : menArr) {
+                    if (chessman.getNumCoordinate() == getNumCoordinate() - i && chessman.getAlphabetCoordinate() == getAlphabetCoordinate() - i)
+                        return false;
+                }
+            }
+
+        }
+
+        return true;
     }
 
     public boolean move(int alpCoordinate, int numCordinate, String[][] field) {
         int n = alpCoordinate - getAlphabetCoordinate();
         if (n < 0)
             n *= -1;
-        if ((getAlphabetCoordinate() - n == alpCoordinate || getAlphabetCoordinate() + n == alpCoordinate) &&(getNumCoordinate() -n == numCordinate || getNumCoordinate() + n == numCordinate)){
+        if ((getAlphabetCoordinate() - n == alpCoordinate || getAlphabetCoordinate() + n == alpCoordinate) && (getNumCoordinate() - n == numCordinate || getNumCoordinate() + n == numCordinate)) {
             field[getAlphabetCoordinate()][getNumCoordinate()] = " ";
             setAlphabetCoordinate(alpCoordinate);
             setNumCoordinate(numCordinate);
