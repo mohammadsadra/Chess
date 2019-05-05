436a4ed82719e901392fa743c4e2cd690bcfa08f
9731077 <9731077@ceit.local>
2019-05-05 17:53:34 +0430

    checker & move added

--- src/Horse.java
+++ src/Horse.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class Horse {
+public class Horse extends Chessman {
+
+    public Horse(String color, int alphabetCoordinate, int numCoordinate) {
+        super(color, alphabetCoordinate, numCoordinate);
+    }
+
+    public boolean check(int alpCoordinate, int numCordinate, ArrayList<Chessman> menArr) {
+        for (Chessman chessman : menArr) {
+            if (chessman.getAlphabetCoordinate() == alpCoordinate && chessman.getNumCoordinate() == numCordinate) {
+                if (chessman.getColor() == getColor())
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean move(int alpCoordinate, int numCordinate, String[][] field) {
+        if ((getNumCoordinate() - 1 == numCordinate && getAlphabetCoordinate() + 2 == alpCoordinate) || (getNumCoordinate() - 2 == numCordinate && getAlphabetCoordinate() + 1 == alpCoordinate) || (getNumCoordinate() - 1 == numCordinate && getAlphabetCoordinate() - 2 == alpCoordinate) || (getNumCoordinate() - 2 == numCordinate && getAlphabetCoordinate() - 1 == alpCoordinate) || (getNumCoordinate() + 1 == numCordinate && getAlphabetCoordinate() + 2 == alpCoordinate) || (getNumCoordinate() + 2 == numCordinate && getAlphabetCoordinate() + 1 == alpCoordinate) || (getNumCoordinate() + 1 == numCordinate && getAlphabetCoordinate() - 2 == alpCoordinate) || (getNumCoordinate() + 2 == numCordinate && getAlphabetCoordinate() - 1 == alpCoordinate)) {
+            field[getAlphabetCoordinate()][getNumCoordinate()] = " ";
+            setAlphabetCoordinate(alpCoordinate);
+            setNumCoordinate(numCordinate);
+            field[getAlphabetCoordinate()][getNumCoordinate()] = getName();
+            return true;
+        }
+        System.out.println("Wrong input");
+        return false;
+    }
 }
