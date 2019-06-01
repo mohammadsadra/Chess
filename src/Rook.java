a88ae2eed65f0836a71cebd107ac69c1ddb772d8
9731077 <9731077@ceit.local>
2019-06-01 11:40:30 +0430

    Rook bug fixed

--- src/Rook.java
+++ src/Rook.java
@@ -1,51 +1,68 @@
-import java.lang.Math;
 import java.util.ArrayList;
 
+/**
+ * @author M.Sadra Haeri
+ * @version 1.0
+ */
 public class Rook extends Chessman {
     public Rook(String color, int alphabetCoordinate, int numCoordinate) {
         super(color, alphabetCoordinate, numCoordinate);
     }
 
+    /**
+     * @param alpCoordinate new position
+     * @param numCordinate  new position
+     * @param menArr        array of chessman
+     * @return if you can move piece return true
+     */
     public boolean check(int alpCoordinate, int numCordinate, ArrayList<Chessman> menArr) {
+        if ((alpCoordinate == getAlphabetCoordinate() || numCordinate == getNumCoordinate()) && !(alpCoordinate == getAlphabetCoordinate() && numCordinate == getNumCoordinate())) {
+        } else
+            return false;
+/////////////////////
         for (Chessman chessman : menArr) {
             if (chessman.getAlphabetCoordinate() == alpCoordinate && chessman.getNumCoordinate() == numCordinate) {
                 if (chessman.getColor() == getColor())
                     return false;
             }
         }
-        if (numCordinate == getNumCoordinate()) {
+/////////////////////////
+        if (numCordinate == getNumCoordinate() && alpCoordinate != getAlphabetCoordinate()) {
+
             int n = alpCoordinate - getAlphabetCoordinate();
-            if(n > 0){
-                for (int i = 0; i != n; i++){
+            if (n == 0)
+                return false;
+            else if (n > 0) {
+                for (int i = 1; i <= n; i++) {
                     for (Chessman chessman : menArr) {
-                        if (chessman.getAlphabetCoordinate() == getAlphabetCoordinate() + i)
+                        if ((chessman.getAlphabetCoordinate() == getAlphabetCoordinate() + i) && chessman.getNumCoordinate() == getNumCoordinate())
+                            return false;
+                    }
+                }
+            } else {
+                for (int i = 1; i <= (n * (-1)); i++) {
+                    for (Chessman chessman : menArr) {
+                        if ((chessman.getAlphabetCoordinate() == getAlphabetCoordinate() - i) && chessman.getNumCoordinate() == getNumCoordinate())
                             return false;
                     }
                 }
             }
-            else {
-                for (int i = 0; i != (n*(-1)); i++){
-                    for (Chessman chessman : menArr) {
-                        if (chessman.getAlphabetCoordinate() == getAlphabetCoordinate() - i)
-                            return false;
-                    }
-                }
-            }
-        }
-        else if(alpCoordinate == getAlphabetCoordinate()){
+        } else if (alpCoordinate == getAlphabetCoordinate()) {
+
             int n = numCordinate - getNumCoordinate();
-            if (n > 0){
-                for (int i = 0; i != n; i++){
+            if (n == 0)
+                return false;
+            else if (n > 0) {
+                for (int i = 1; i <= n; i++) {
                     for (Chessman chessman : menArr) {
-                        if (chessman.getNumCoordinate() == getNumCoordinate() + i)
+                        if ((chessman.getNumCoordinate() == getNumCoordinate() + i) && chessman.getAlphabetCoordinate() == getAlphabetCoordinate())
                             return false;
                     }
                 }
-            }
-            else {
-                for (int i = 0; i != (n * -1); i++){
+            } else {
+                for (int i = 1; i <= (n * -1); i++) {
                     for (Chessman chessman : menArr) {
-                        if (chessman.getNumCoordinate() == getNumCoordinate() - i)
+                        if ((chessman.getNumCoordinate() == getNumCoordinate() - i) && chessman.getAlphabetCoordinate() == getAlphabetCoordinate())
                             return false;
                     }
                 }
@@ -55,15 +72,5 @@ public class Rook extends Chessman {
         return true;
     }
 
-    public boolean move(int alpCoordinate, int numCordinate, String[][] field) {
-        if ((alpCoordinate == getAlphabetCoordinate() || numCordinate == getNumCoordinate()) /*&& ((alpCoordinate != getAlphabetCoordinate() && numCordinate != getNumCoordinate()) */) {
-            field[getAlphabetCoordinate()][getNumCoordinate()] = " ";
-            setAlphabetCoordinate(alpCoordinate);
-            setNumCoordinate(numCordinate);
-            field[getAlphabetCoordinate()][getNumCoordinate()] = getName();
-            return true;
-        }
-        System.out.println("Wrong input");
-        return false;
-    }
+
 }
